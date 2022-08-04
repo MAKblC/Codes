eba9ab65bc77251d7f58a541c5ab67e89ab3490e
MAKblC <max.4444@mail.ru>
2022-08-04 13:22:28 +0300

    changed sketch

--- MGS-GUVA/MGS-GUVA.ino
+++ MGS-GUVA/MGS-GUVA.ino
@@ -8,12 +8,19 @@ void setup() {
   Serial.begin(115200);
   // Инициализация I2C интерфейса
   Wire.begin();
+  mcp3221.setVinput(VOLTAGE_INPUT_5V);
 }
 
 void loop()
 {
-  float adc0 = mcp3221.getVoltage();
-  Serial.println(adc0);
+  float sensorVoltage;
+  float sensorValue;
+  float UV_index;
+
+  sensorValue = mcp3221.getVoltage();
+  sensorVoltage = 1000 * (sensorValue / 4096 * 5.0); // напряжение на АЦП
+  UV_index = 370 * sensorVoltage / 200000; // Индекс УФ (эмпирическое измерение)
+  Serial.println("Выходное напряжение = " + String(sensorVoltage, 1) + "мВ");
+  Serial.println("UV index = " + String(UV_index, 1));
   delay(200);
 }
-
