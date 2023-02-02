8c6acafcf67917d8b0a9dfe1c1e91fee609fb0de
MAKblC <max.4444@mail.ru>
2023-02-02 10:40:56 +0300

    added value print

--- MGB-LCD12864/MGB-LCD12864.ino
+++ MGB-LCD12864/MGB-LCD12864.ino
@@ -1,12 +1,17 @@
-// переменный резистор на задней стороне для регулировки контраста
+// переменный резистор R4 на задней стороне для регулировки контраста
 
 #include <Wire.h>
 #include <I2C_graphical_LCD_display.h>
 I2C_graphical_LCD_display lcd;
 
+int val = 0;
+float b = 0;
+char b1[8];
+char val2[8];
+
 void setup ()
 {
-  // Инициализация дисплея
+   // Инициализация дисплея
   lcd.begin();
 
   // Рисование рамки
@@ -16,10 +21,19 @@ void setup ()
   lcd.clear (10, 10, 20, 20, 0xFF);
 
   // Вывод строки
-  lcd.gotoxy (10, 50);
+  lcd.gotoxy (10, 50); // координата курсора
   lcd.string ("Privet, MGBOT!", false);
 }
 
 void loop ()
 {
+  val = hallRead(); // считывание значеия датчика Холла
+  itoa(val, val2, 10); // перевод int в char
+  lcd.gotoxy (10, 30); 
+  lcd.string(val2); 
+  lcd.gotoxy (40, 10);
+  b = b + 0.3; // инкремент значения
+  dtostrf(b, 6, 2, b1);  // перевод float в char
+  lcd.string(b1);
+  delay(1000);
 }
