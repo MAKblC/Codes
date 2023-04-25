eaa954af025bb7c553e7eb64acf99f76119b5f17
MAKblC <max.4444@mail.ru>
2023-04-25 16:41:11 +0300

    update library

--- MGS-L75/MGS-L75.ino
+++ MGS-L75/MGS-L75.ino
@@ -1,20 +1,19 @@
+
+#include <BH1750.h>
 #include <Wire.h>
-#include <BH1750FVI.h>
-BH1750FVI LightSensor_1;
+
+BH1750 lightMeter;
 
 void setup() {
-  // Инициализация последовательного порта
-  Wire.begin();
   Serial.begin(115200);
-  // Инициализация датчика
-  LightSensor_1.begin(); // (0x23), (0x5C) (также попробуйте просканировать адрес: https://github.com/MAKblC/Codes/tree/master/I2C%20scanner)
-  LightSensor_1.setMode(Continuously_High_Resolution_Mode);
+  Wire.begin();
+  lightMeter.begin();
 }
 
 void loop() {
-  // Измерение
-  float l = LightSensor_1.getAmbientLight();
-  // Вывод измеренных значений в терминал
-  Serial.println("Ambient light intensity: " + String(l, 1) + " lx");
-  delay(250);
+  float lux = lightMeter.readLightLevel(); // считывание освещенности
+  Serial.print("Освещенность: ");
+  Serial.print(lux);
+  Serial.println(" Люкс");
+  delay(1000);
 }
