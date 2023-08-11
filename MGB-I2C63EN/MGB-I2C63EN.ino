d634cee25da740761f3f47e2522e447635057592
MAKblC <max.4444@mail.ru>
2023-08-11 12:29:37 +0300

    added comment

--- MGB-I2C63EN/MGB-I2C63EN.ino
+++ MGB-I2C63EN/MGB-I2C63EN.ino
@@ -86,8 +86,8 @@ bool setBusChannel(uint8_t i2c_channel)
   else
   {
     Wire.beginTransmission(I2C_HUB_ADDR);
-    Wire.write(i2c_channel | EN_MASK);
-	// Wire.write(0x01 << i2c_channel);
+    Wire.write(i2c_channel | EN_MASK); // для микросхемы PCA9547
+	// Wire.write(0x01 << i2c_channel); // Для микросхемы PW548A
     Wire.endTransmission();
     return true;
   }
