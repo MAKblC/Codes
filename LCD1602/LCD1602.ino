d0be0932dfd6a101f463aa7107626ce844a5d21b
MAKblC <max.4444@mail.ru>
2023-08-28 17:36:16 +0300

    edited sketch

--- LCD1602/LCD1602.ino
+++ LCD1602/LCD1602.ino
@@ -5,7 +5,7 @@ int lcdColumns = 16;
 int lcdRows = 2;
 
 // добавляем экзмпляр (адрес, количество столбцов, количество строк)
-LiquidCrystal_I2C lcd(0x3F, lcdColumns, lcdRows); // 0x27 (также попробуйте просканировать адрес: https://github.com/MAKblC/Codes/tree/master/I2C%20scanner)
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows); // 0x3F (также попробуйте просканировать адрес: https://github.com/MAKblC/Codes/tree/master/I2C%20scanner)
 
 void setup() {
   // запуск LCD
